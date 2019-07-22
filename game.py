2e719ba6e12f653d33cf4162edfb5df9860b9136
malya727 <niranjanmalya5@gmail.com>
2019-07-22 10:26:15 +0530

    Internship Files

---
+++ game.py
@@ -0,0 +1,25 @@
+''' program for dice game '''
+
+import random
+
+q=0
+print("********** Welcome to my game **********")
+while(q==0):
+    n=random.randint(1,6)
+    at=1
+    while(True):
+        inp=int(input("Enter guessed Number :"))
+        if(at>3):
+            print("Oh!! Sorry You lost your chances")
+            print(f"Generated number is {n} ")
+            break
+        else:
+            if(n==inp):
+                print(f"Congrats You guessed in {at} attempt")
+                break
+            else:
+                at+=1
+    p=input("Want to Continue game (y/n)")
+    if(p=='n'):
+        q=1
+
